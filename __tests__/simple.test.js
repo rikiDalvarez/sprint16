2ce61be9ca155117455b18e989021e85577c83e4
Ricardo <ric.dalvarez@hotmail.com>
2023-05-22 14:08:38 +0200

    simple testing working

---
+++ __tests__/simple.test.js
@@ -0,0 +1,5 @@
+const { add, subtract, multiply, devide } = require("../app/simple")
+
+test('adds 1 + 2 to equal 3', () => {
+	expect(add(1, 2)).toBe(3);
+});
---
+++ app/simple.js
@@ -0,0 +1,17 @@
+function add(number, number1) {
+	return number + number1
+}
+
+function subtract(number, number1) {
+	return number - number1
+}
+
+function multiply(number, number1) {
+	return number * number1
+}
+
+function devide(number, number1) {
+	return number / number1
+}
+
+module.exports = { add, subtract, multiply, devide }
