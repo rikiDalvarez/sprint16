e429b1c98824d78586c29f490b2ebf1f9a4f4a77
Ricardo <ric.dalvarez@hotmail.com>
2023-05-23 10:35:59 +0200

    test 1.2

--- __tests__/simple.test.js
+++ __tests__/simple.test.js
@@ -3,3 +3,15 @@ const { add, subtract, multiply, devide } = require("../app/simple")
 test('adds 1 + 2 to equal 3', () => {
 	expect(add(1, 2)).toBe(3);
 });
+
+test("subtract 4 - 2 to equal 2", () => {
+	expect(subtract(4, 2)).toBe(2)
+})
+
+test("multiply 2 *3  to equal 6", () => {
+	expect(multiply(2, 3)).toBe(6)
+})
+
+test("subtract 4/2 to equal 2", () => {
+	expect(devide(4, 2)).toBe(2)
+})
---
+++ app/promise.js
@@ -0,0 +1,16 @@
+function returnPromise(string) {
+	return new Promise((res, rej) => {
+		if (string.length > 5) {
+			res("large name")
+		} else {
+			rej(new Error("too short name"))
+		}
+	})
+}
+
+let arrFunc = (variable, func) => {
+	if (variable)
+		return func(variable);
+}
+
+module.exports = { returnPromise, arrFunc }
